08e52f74a0da41c937af69e247bf49157276641a
Miroslav Paralyuzov <137619754+paralyuzov@users.noreply.github.com>
2025-06-25 18:07:19 +0300

    feat: configure NgRx in app config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,22 +1,43 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
 import { providePrimeNG } from 'primeng/config';
 import Lara from '@primeng/themes/lara';
 import { routes } from './app.routes';
+
+import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { cpuReducer } from './store/cpu/cpu.reducer';
+import { CpuEffects } from './store/cpu/cpu.effects';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
+    provideHttpClient(),
+    provideStore({
+      cpu: cpuReducer,
+    }),
+
+    provideEffects([CpuEffects]),
+
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: false,
+      trace: false,
+      traceLimit: 75,
+    }),
+
     providePrimeNG({
       theme: {
         preset: Lara,
         options: {
           darkModeSelector: false,
-          cssLayer: false
-        }
+          cssLayer: false,
+        },
       },
     }),
   ],
